021dab074b7813097551290c7c95840e7a4dccb3
SedativeGlam <winnieonwuka2019@gmail.com>
2025-12-08 23:24:17 +0100

    feat: Add demo section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,4 @@
+import DemoSection from "@/components/DemoSection";
 import Features from "@/components/Features";
 import HeroSection from "@/components/HeroSection";
 import HowItWorks from "@/components/HowItWorks";
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <Features />
       <HowItWorks />
+      <DemoSection />
     </div>
   );
 }
